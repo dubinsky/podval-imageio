189578a6071c6bf8bdc7ed33bc474f7658e951d3
Leonid Dubinsky <dub@podval.org>
2009-04-12 00:24:53 -0400

    Switched meta-meta-data reading to pool-parser.

---
+++ src/main/java/org/podval/imageio/metametadata/loader/MetaMetaDataLoader.java
@@ -0,0 +1,65 @@
+package org.podval.imageio.metametadata.loader;
+
+import org.podval.imageio.metametadata.MetaMetaData;
+
+import org.podval.imageio.metametadata.MetaMetaDataException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamException;
+
+
+public final class MetaMetaDataLoader {
+
+    public MetaMetaDataLoader(final MetaMetaData metaMetaData) {
+        this.metaMetaData = metaMetaData;
+    }
+
+
+    public void load(final String resourceName) throws XMLStreamException, IOException, MetaMetaDataException {
+//        final InputStream is = getClass().getClassLoader().getResourceAsStream(resourceName);
+        final InputStream is = new FileInputStream(resourceName);
+        if (is == null) {
+            throw new IOException("Can't find resource " + resourceName);
+        }
+
+        if (resourceName.endsWith(".list")) {
+            loadList(is);
+
+        } else {
+            loadXml(is);
+        }
+
+        is.close();
+    }
+
+
+    private void loadList(final InputStream is) throws XMLStreamException, IOException, MetaMetaDataException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            line = line.trim();
+            if ((line.length() > 0) && !line.startsWith("#")) {
+                load(line);
+            }
+        }
+    }
+
+
+    private void loadXml(final InputStream is) throws XMLStreamException, MetaMetaDataException {
+        final XMLStreamReader in = XMLInputFactory.newInstance().createXMLStreamReader(is);
+        new Loader(in, metaMetaData).load();
+    }
+
+
+    private final MetaMetaData metaMetaData;
+}
